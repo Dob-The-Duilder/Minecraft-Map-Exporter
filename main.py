f0a9169b9720e51b943e99b32523443278202783
BobTheBuilder <58479577+kilometerbob@users.noreply.github.com>
2022-10-06 20:34:54 -0600

    Updated to contain multiprocessing
    Program runs very fast now but some features had to be temporarily disabled(Merging and Extra map icons)

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-import tkinter, os, sys, time, threading
+import tkinter, os, sys, time, threading, time
 from tkinter import Tk, PhotoImage, constants, filedialog, messagebox
 from tkinter import *
 import tkinter as tk
 from PIL import Image, ImageTk
+from itertools import repeat
+from multiprocessing import Pool
+from multiprocessing.dummy import Pool as ThreadPool
 
 import mapGen
-
-ran = 0
 
 def error():
    messagebox.showerror("Error", "Sorry, this is still a work in progress >.<")
@@ -14,21 +15,25 @@ def error():
 def GenerateMaps():
     global settingsList
     
-    var = mapGen.makeMaps(settingsList[1][1], filedialog.askopenfilenames(initialdir = settingsList[0][1], parent=mainScreen, title='Choose a file'), settingsList[2][1], settingsList[3][1])
-    print(str(var[3]))
+    fileList = filedialog.askopenfilenames(initialdir = settingsList[0][1], parent=mainScreen, title='Choose a file')
+    
+    t0 = time.time()
+    with Pool() as pool:
+        results = pool.starmap(mapGen.makeMaps, zip(repeat(settingsList[1][1]), fileList, repeat(settingsList[2][1]), repeat(settingsList[3][1])))
+    t1 = time.time()
+    print(t1-t0)
+    var = results[0]
     setBackground(var[0],var[1],var[2])
 
 def setBackground(X,Z,bigImg):
-    global ran
     global bg
-    global canvas
-    if ran == 0:
-        canvas = tk.Canvas(mainScreen, width=X*2, height=Z*2)
-    else:
+    try:
         canvas.delete(bg)
-        canvas.config(width=X*2, height=Z*2)
+    except:
+        pass
+    
+    canvas.config(width=X*2, height=Z*2)
     canvas.pack(side = 'top')
-    ran = 1
     
     if (Z*2+30 <= 1080):
         mainScreen.geometry('{}x{}'.format((X*2), (Z*2+30)))
@@ -66,55 +71,76 @@ def defineSettings():
         line = str(setting[0]) + '=' + str(setting[1]) + '\n'
         settingsFile.write(line)
     settingsFile.close()
-
-def reload():
-    import mapGen
     
-path = os.path.abspath(__file__)
-local = path.replace(os.path.basename(path), '')
+def newSettings():
+     
+    # Toplevel object which will
+    # be treated as a new window
+    settingsWindow = Toplevel(mainScreen)
+ 
+    # sets the title of the
+    # Toplevel widget
+    settingsWindow.title("Settings")
+    settingsWindow.iconphoto(False, icon)
+ 
+    # sets the geometry of toplevel
+    settingsWindow.geometry("200x200")
+ 
+    # A Label widget to show in toplevel
+    Label(settingsWindow,
+          text ="This is a new window").pack()
 
-if not os.path.exists(local + 'settings.txt'):
-    with open(os.path.join(local, 'settings.txt'), 'w') as fp:
-        pass
+if __name__ == "__main__":    
+    path = os.path.abspath(__file__)
+    local = path.replace(os.path.basename(path), '')
 
-settingsFile = open('settings.txt', 'r')
-settingsList = [line.replace('\n', '').split('=') for line in settingsFile.readlines()]
-settingsFile.close()
+    if not os.path.exists(local + 'settings.txt'):
+        with open(os.path.join(local, 'settings.txt'), 'w') as fp:
+            pass
 
-mainScreen = Tk()
+    settingsFile = open('settings.txt', 'r')
+    settingsList = [line.replace('\n', '').split('=') for line in settingsFile.readlines()]
+    settingsFile.close()
+    
+    mainScreen = Tk()
 
-#icon = PhotoImage(file = (local + "Graphics\Icon.png"))
+    icon = PhotoImage(file = (local + "Graphics\Icon.png"))
+    back = Image.open(local + "Graphics\Icon.png")
 
-NavBar = Frame(mainScreen)
-NavBar.pack(fill=X)
+    NavBar = Frame(mainScreen)
+    NavBar.pack(fill=X)
 
-btn = Button(NavBar, text = 'Exit', command = mainScreen.destroy)
-btn.pack(side = 'left')
+    btn = Button(NavBar, text = 'Exit', command = mainScreen.destroy)
+    btn.pack(side = 'left')
 
-mapbtn = Button(NavBar, text = 'Generate Maps', command = GenerateMaps)
-mapbtn.pack(side = 'left')
+    mapbtn = Button(NavBar, text = 'Generate Maps', command = GenerateMaps)
+    mapbtn.pack(side = 'left')
 
-btn2 = Button(NavBar, text="Settings", command = defineSettings)
-btn2.pack(side = 'left')
+    btn2 = Button(NavBar, text="Settings", command = defineSettings)
+    btn2.pack(side = 'left')
 
-btn3 = Button(NavBar, text="Reload", command = reload)
-btn3.pack(side = 'left')
+    btn2 = Button(NavBar, text="Settings", command = newSettings)
+    btn2.pack(side = 'left')
 
-mainScreen.attributes('-alpha')
-#mainScreen.iconphoto(False, icon) 
-mainScreen.title("  Minecraft Exporter")
-mainScreen.configure(background = 'black')
-mainScreen.geometry("450x500")
-mainScreen.resizable(width=True, height=True)
 
-#background = Image.open('Graphics\Icon.png')
-#setBackground(128,128, background)
+    mainScreen.attributes('-alpha')
+    mainScreen.iconphoto(False, icon) 
+    mainScreen.title("  Minecraft Exporter")
+    mainScreen.configure(background = 'black')
+    mainScreen.geometry("450x500")
+    mainScreen.resizable(width=True, height=True)
 
-if (len(settingsList) < 4):
-    messagebox.showerror("Error", "Settings need to be generated before program can run")
-    defineSettings()
-elif (settingsList[3][1] == ''):
-    messagebox.showerror("Error", "Settings need to be generated before program can run")
-    defineSettings()
+    canvas = tk.Canvas(mainScreen, width=256, height=256)
+    setBackground(256,256,back)
 
-mainScreen.mainloop()
+    #background = Image.open('Graphics\Icon.png')
+    #setBackground(128,128, background)
+
+    if (len(settingsList) < 4):
+        messagebox.showerror("Error", "Settings need to be generated before program can run")
+        defineSettings()
+    elif (settingsList[3][1] == ''):
+        messagebox.showerror("Error", "Settings need to be generated before program can run")
+        defineSettings()
+
+    mainScreen.mainloop()
